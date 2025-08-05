46bac439a31b469d3af3b7012196328eb6c3a4c3
nivedita-watpade <niveditawatpade298@gmail.com>
2025-08-05 23:27:00 +0530

    DOM AND DOM manipulation

--- 02-Fundamentals-Part-2/starter/newScript.js
+++ 02-Fundamentals-Part-2/starter/newScript.js
@@ -13,6 +13,15 @@
 
 // // codewars.com;
 
+// Four steps to solve problems
+// 1)100% understand the problem. Ask the right question to clear the problem
+
+// 2)Divide and Conquer: Divide big problem into smaller sub problems
+
+// 3)Research: Do research as much you have
+
+// 4)Write Pseudo Code: Pseudo-code is simpley informal description of actual code
+
 const obj = {
   parentKey: {
     nestedKey: {
