84eaa1865d4c852337c7381cf34690efb4485f4c
nivedita-watpade <niveditawatpade298@gmail.com>
2026-01-08 19:33:37 +0530

    geolaction api

---
+++ 15-Mapty/starter/Notes.md
@@ -0,0 +1,14 @@
+Geolocation API (JavaScript)
+What is Geolocation API?
+
+The Geolocation API allows a web application to get the user’s current geographic location (latitude & longitude) using the browser.
+
+Syntax: navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
+
+Ex. navigator.geolocation.getCurrentPosition(
+function (position) {
+const { latitude, longitude } = position.coords;
+console.log(latitude, longitude);
+},
+() => alert('Could not get your position!')
+);
--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,15 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      console.log(new Date(position.timestamp));
+      const { latitude, longitude } = position.coords;
+      console.log(latitude, longitude);
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+    () => alert('Could not get your position!')
+  );
