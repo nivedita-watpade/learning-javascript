cd7315126ac7190b798138810154aa7611ed67eb
nivedita-watpade <niveditawatpade298@gmail.com>
2026-01-23 11:47:56 +0530

    Ajax call in practice

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -10,3 +10,54 @@ const countriesContainer = document.querySelector('.countries');
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
+
+//Old way
+
+function countryData(country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    //console.log(this.responseText);
+    const data = JSON.parse(this.responseText);
+    console.log(data);
+
+    data.forEach(countryObj => {
+      displayCountry(countryObj);
+    });
+  });
+}
+
+countryData('India');
+// countryData('US');
+
+function displayCountry(data) {
+  const html = `<article class="country">
+          <img class="country__img" src=${data.flag} />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${populationFormatter(data.population, data.alpha2Code)} people</p>
+            <p class="country__row"><span>🗣️</span>${getLangauges(data.languages)}</p>
+            <p class="country__row"><span>💰</span>${getCurrencies(data.currencies)}</p>
+          </div>
+        </article>`;
+
+  countriesContainer.insertAdjacentHTML('afterbegin', html);
+}
+
+function getLangauges(langArr) {
+  return langArr.map(lang => lang.name).join(', ');
+}
+
+function getCurrencies(currArr) {
+  return currArr.map(currency => currency.name).join(', ');
+}
+
+function populationFormatter(population, alpha2Code) {
+  const formattedPop = new Intl.NumberFormat(`en-${alpha2Code}`).format(
+    population,
+  );
+  return formattedPop;
+}
